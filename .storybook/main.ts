97ec862a1caf8a097118fa00205d92c6d8bb32f9
bswitzer <brenna.switzer@gmail.com>
2025-04-12 13:45:27 -0700

    add vitest and storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,19 @@
+import { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-essentials",
+  ],
+  "framework": {
+    "name": "@storybook/nextjs",
+    "options": {}
+  },
+  "staticDirs": [
+    "../public"
+  ]
+};
+export default config;
