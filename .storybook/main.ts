fdb6ea99bce9b853b403879244ce3e390a004a38
bswitzer <brenna.switzer@gmail.com>
2025-04-12 15:35:39 -0700

    configure vitest

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,12 @@
-import { StorybookConfig } from '@storybook/nextjs';
+import { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-essentials",
-  ],
-  "framework": {
-    "name": "@storybook/nextjs",
-    "options": {}
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: ["@storybook/addon-essentials"],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
   },
-  "staticDirs": [
-    "../public"
-  ]
+  staticDirs: ["../public"],
 };
 export default config;
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,15 @@
+import { resolve } from "node:path";
 import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    environment: 'jsdom',
     exclude: [...configDefaults.exclude],
+    globals: true,
+    include: ['./**/*.test.tsx'],
+    setupFiles: "./src/test/setup.ts", 
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
   },
 });
