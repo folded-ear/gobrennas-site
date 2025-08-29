301ba9b592011369d65072d0034d52bc3d5ad21e
Barney Boisvert <bboisvert@gmail.com>
2025-08-29 15:10:51 -0700

    add missing /login page

---
+++ src/app/(public)/login/page.tsx
@@ -0,0 +1,11 @@
+import Login from "@/components/views/login";
+
+export default function LoginPage() {
+  return (
+    <div className="flex flex-col h-dvh">
+      <main className="flex-1 p-lg">
+        <Login />
+      </main>
+    </div>
+  );
+}
