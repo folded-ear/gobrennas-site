242be004cf3e5801e124380c3866a1168004bb4f
Barney Boisvert <bboisvert@gmail.com>
2026-01-24 11:48:21 -0800

    if you hit the homepage while logged in, redirect to /recipes

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,8 +1,16 @@
 import { Container } from "@/components/ui/layout";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import Login from "@/components/views/login";
+import { GET_PROFILE } from "@/data/get-profile";
+import { query } from "@/lib/apollo-client";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  let data;
+  try {
+    data = await query({ query: GET_PROFILE });
+  } catch {}
+  if (data?.data?.profile.me.email) redirect("/recipes");
   return (
     <div className="flex flex-col h-dvh">
       <div className="p-lg flex justify-end ">
