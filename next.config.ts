7f7bc2c9c20087f5e1e232ee0aede314abd6ae0a
Barney Boisvert <bboisvert@gmail.com>
2026-01-27 13:37:28 -0800

    add an opt-in ENV config for using Next's image cache for user avatars

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,22 @@
+import { loadEnvConfig } from "@next/env";
 import type { NextConfig } from "next";
+
+const projectDir = process.cwd();
+loadEnvConfig(projectDir);
+
+const remoteImagePatterns = [];
+if (process.env.NEXT_PUBLIC_CACHE_USER_AVATARS === "true") {
+  remoteImagePatterns.push({
+    hostname: "*.googleusercontent.com",
+  });
+}
 
 const nextConfig: NextConfig = {
   output: "standalone",
   poweredByHeader: false,
+  images: {
+    remotePatterns: remoteImagePatterns,
+  },
   async redirects() {
     return [
       {
