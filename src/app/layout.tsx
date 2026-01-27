471c7f5a48660c1cc617383a1756bebc58319a83
Barney Boisvert <bboisvert@gmail.com>
2026-01-27 13:19:19 -0800

    add mobile viewport meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { graphqlUri } from "@/app/(public)/constants";
 import ErrorFallback from "@/components/views/error-fallback";
 import { ApolloWrapper } from "@/lib/apollo-wrapper";
 import { ThemeProvider } from "@/providers/theme-provider";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { CookiesProvider } from "next-client-cookies/server";
 import { ErrorBoundary } from "react-error-boundary";
 import "./globals.css";
@@ -10,6 +10,15 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Brenna's Food Software",
   description: "Your _face_ is a food software!",
+};
+
+export const viewport: Viewport = {
+  userScalable: false,
+  width: "device-width",
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
+  themeColor: "#F57F17", // duplicated in styles
 };
 
 export default async function RootLayout({
