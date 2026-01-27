0609797659d68ffb7bcc84ced8ad739f1a7ca609
Barney Boisvert <bboisvert@gmail.com>
2026-01-27 13:37:28 -0800

    add an opt-in ENV config for using Next's image cache for user avatars

--- src/components/ui/user-avatar.tsx
+++ src/components/ui/user-avatar.tsx
@@ -1,5 +1,7 @@
 import { User } from "@/__generated__/graphql";
 import { Avatar, AvatarRootProps } from "@heroui/react";
+import { getImageProps } from "next/image";
+import { useMemo } from "react";
 
 interface Props {
   user: Pick<User, "name" | "email" | "imageUrl">;
@@ -8,9 +10,27 @@ interface Props {
 
 export default function UserAvatar({ user, size = "sm" }: Props) {
   const name = user.name || user.email;
+  const imgProps = useMemo(() => {
+    if (
+      !user.imageUrl ||
+      process.env.NEXT_PUBLIC_CACHE_USER_AVATARS !== "true"
+    ) {
+      return {
+        src: user.imageUrl ?? undefined,
+        alt: name,
+      };
+    }
+    const { props } = getImageProps({
+      src: user.imageUrl,
+      alt: name,
+      fill: true,
+      sizes: "48px",
+    });
+    return props;
+  }, [name, user.imageUrl]);
   return (
     <Avatar size={size} title={name}>
-      <Avatar.Image src={user.imageUrl || undefined} alt={name} />
+      <Avatar.Image {...imgProps} />
       <Avatar.Fallback>
         {name
           .split(/\s+/)
